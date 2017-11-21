a08c824694a273df508c63aee65390e2072bffa4
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-11-20 18:03:40 -0600

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './Form.js';
+
+//Intro page with nested form for login, info.
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Main Page</h1>
+        </header>
+        <Form />
+        <p className="App-intro">
+          This is where the info will go.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Main;
